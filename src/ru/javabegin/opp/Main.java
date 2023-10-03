24997ffae095052a33ed8b605a9a334d51b46b26
Khamul1 <rositlsav.kostencko@gmail.com>
2023-10-03 20:24:45 +0300

    Сlasses and objects

--- src/ru/javabegin/opp/Main.java
+++ src/ru/javabegin/opp/Main.java
@@ -3,7 +3,10 @@ package ru.javabegin.opp;
 public class Main {
 
     public static void main(String[] args) {
-        Car car = new Car();
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        Car car4 = new Car();
 
     }
 }
