bc554e1868f6771ce73ccfd5ff30652346743074
Khamul1 <rositlsav.kostencko@gmail.com>
2023-10-03 20:10:35 +0300

    Creating an object

---
+++ src/ru/javabegin/opp/Car.java
@@ -0,0 +1,4 @@
+package ru.javabegin.opp;
+
+public class Car {
+}
---
+++ src/ru/javabegin/opp/Main.java
@@ -0,0 +1,9 @@
+package ru.javabegin.opp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car car = new Car();
+
+    }
+}
