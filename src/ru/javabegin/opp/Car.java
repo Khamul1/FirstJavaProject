499ae31d5eceeb2a675eee5ab5ed539a2f4adb00
Khamul1 <rositlsav.kostencko@gmail.com>
2023-10-07 21:56:26 +0300

    Getters and setters

--- src/ru/javabegin/opp/Car.java
+++ src/ru/javabegin/opp/Car.java
@@ -2,15 +2,39 @@ package ru.javabegin.opp;
 
 public class Car {
 
-    String name;
-    String model;
-    String color;
+    private String name;
+    private String model;
+    private String color;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     void drive(){ // данный метод ничего не возращает
-        System.out.println(name + " drive"); // конкатенация строк (соединение строк)
+//        System.out.println(name + " drive"); // конкатенация строк (соединение строк)
     }
 
     void stop(){ //данный метод ничего не возращает
-        System.out.println("stop");
+//        System.out.println("stop");
     }
 }
--- src/ru/javabegin/opp/Main.java
+++ src/ru/javabegin/opp/Main.java
@@ -6,20 +6,15 @@ public class Main {
         // переменные ссылаются на объекты
 
         Car nissan = new Car(); // через ссылки обращаемся к объекту и можем изменять его свойства
-        nissan.color = "blue";
-//        nissan.name = "nissan almera";
-        nissan.model = "sedan";
+        nissan.setColor("blue");
+        nissan.setName("nissan almera");
+        nissan.setModel("sedan");
         nissan.drive();
 
-        Car bmw = new Car();
-        bmw.color = "black";
-//        bmw.name = "bmw x5";
-        bmw.model = "crossover";
-        bmw.drive();
 
         new Car(); // Объект без ссылки( не сможем к нему обратиться)
 
-//        System.out.println(nissan.color); // sout + Tab
+        System.out.println(nissan.getName()); // sout + Tab
 //        System.out.println(bmw.color);
 
 
