9306753718d5d638e83c9b6387b0316b9826e31a
Khamul1 <rositlsav.kostencko@gmail.com>
2023-10-12 13:29:03 +0300

    super

--- src/ru/javabegin/opp/Car.java
+++ src/ru/javabegin/opp/Car.java
@@ -15,4 +15,14 @@ public class  Car extends Transport{
     public  void  signal(){
         System.out.println("signal");
     }
+
+    @Override
+    void drive() {
+        System.out.println("car drive");
+    }
+
+    @Override
+    void stop() {
+        System.out.println("car stop");
+    }
 }
