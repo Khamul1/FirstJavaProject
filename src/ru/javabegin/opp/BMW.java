ca82797f034471b96452429e09764fba6450b5cb
Khamul1 <rositlsav.kostencko@gmail.com>
2023-10-21 18:02:17 +0300

    Calling constructors along a chain

--- src/ru/javabegin/opp/BMW.java
+++ src/ru/javabegin/opp/BMW.java
@@ -1,8 +1,17 @@
 package ru.javabegin.opp;
 
 public class BMW extends Car {
+
+    public BMW(String name, String model, String color, int number) {
+        super(name, model, color, number); //должен вызвать конструктор у ПЕРВОГО родителя
+    }
+
+    public BMW() {
+    }
+
     @Override
     public void drive() {
+        super.signal(); // bmw должен просигналить перед тем как поехать
         super.drive();
         System.out.println("drive bmw");    // Данный метод переопределяет Все родит. Методы drive
     }
